40a13319f7b1e180eb492ef63fe12eb93d2e8185
EQuimper <quimperemanuel@gmail.com>
2016-07-09 17:47:10 -0600

    Add transition for route + design home page

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('portfolio');
+  this.route('services');
+  this.route('contact');
 });
 
 export default Router;
