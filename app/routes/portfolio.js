868bb015ddc3ce7b2284b0e33356ae872181cfa6
EQuimper <quimperemanuel@gmail.com>
2016-07-10 00:00:58 -0600

    Add design for showcase page

--- app/routes/portfolio.js
+++ app/routes/portfolio.js
@@ -1,4 +1,51 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    let project = [
+      {
+        'title': 'GameStreaming',
+        'description': `GameStreaming is a application build with the Twitch's Api`,
+        'features': [
+          'Follow a games',
+          'Follow a streamer',
+          'See your favorited',
+          'Fetch the live chat using iFrame'
+        ],
+        'images':[
+          '/assets/images/gamestreaming-image4.png',
+          '/assets/images/gamestreaming-image3.png',
+          '/assets/images/gamestreaming-image2.png',
+          '/assets/images/gamestreaming-image1.png',
+        ],
+        'url': 'http://equimper.github.io/streamingAppRedux/',
+        'technologies': [
+          'React',
+          'Redux',
+          'Webpack',
+          'Material-Ui'
+        ]
+      },
+      {
+        'title': 'Simpletify',
+        'description': `Simpletify is a little app build with the Spotify's Api. Was build for learning jQuery`,
+        'features': [
+          'Fetch a 30s music previews',
+          'Search a artist',
+          'Search a song'
+        ],
+        'images': [
+          '/assets/images/simpletify-image1.png',
+          '/assets/images/simpletify-image3.png',
+          '/assets/images/simpletify-image2.png'
+        ],
+        'url': 'http://equimper.github.io/thinkful-Simpletify/',
+        'technologies': [
+          'jQuery',
+          'Bootstrap'
+        ]
+      }
+    ];
+    return project;
+  }
 });
